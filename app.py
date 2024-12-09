4f9f74833e3d9b295ad2f87c84d57d8ce93a93e5
DARSHAN-V-G <148107860+DARSHAN-V-G@users.noreply.github.com>
2024-12-09 23:11:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,22 @@ from faiss_handler import store_data_in_faiss, answer_query
 from dotenv import load_dotenv 
 
 load_dotenv() 
+pdf_path = "events.pdf"
+index_file = "vector_db.pkl"
 
-db = None
 
 app = Flask(__name__) 
  
 @app.route('/query', methods=['POST']) 
 def query(): 
-    global db
     try: 
         query_text = request.json.get('query', None) 
          
         if not query_text: 
             return jsonify({"error": "No query provided"}), 400 
-         
-        if db is None:
-            db = store_data_in_faiss("events.pdf")
-         
-        results = answer_query(db, query_text) 
-        print(f"Results: {results}") 
+    
+        results = answer_query(query_text,index_file,pdf_path) 
+        print(f"Results : {results}") 
         # Return the search results as JSON response 
         return jsonify(results), 200 
     except Exception as e: 
@@ -29,12 +26,9 @@ def query():
  
 @app.route('/store', methods=['GET']) 
 def store(): 
-    global db
     try: 
         # Store the text data in Faiss only if not already stored
-        if db is None:
-            db = store_data_in_faiss("events.pdf")
-         
+        store_data_in_faiss(pdf_path,index_file)
         return jsonify({"message": "Text data successfully stored!"}), 200 
     except Exception as e: 
         return jsonify({"error": str(e)}), 500 
